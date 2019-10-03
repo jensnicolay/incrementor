2d8fa7db9726b9bfb10d97fb038ca79adc7a96de
Jens Van der Plas <jevdplas@vub.be>
2019-10-03 08:42:07 +0200

    Small changes to naive solver

--- incrementor.rkt
+++ incrementor.rkt
@@ -11,6 +11,7 @@
 
 ; Naming
 ; * P   the set of rules
+; * E   the set of facts
 
 (provide (all-defined-out))
 
@@ -339,22 +340,24 @@
 
   (define (solve-naive-helper rules tuples)
     (let loop ((rules rules) (derived-tuples tuples))
-      (if (set-empty? rules) 
+      (if (set-empty? rules) ; Check whether all rules have been traversed as far as needed.
           derived-tuples
           (let ((rule (set-first rules)))
-            (let ((derived-tuples-for-rule (fire rule derived-tuples (set))))
-              (loop (set-rest rules) (set-union derived-tuples derived-tuples-for-rule)))))))
+            (let ((derived-tuples-for-rule (fire rule derived-tuples (set)))) ; Fire the first rule.
+              (loop (set-rest rules) (set-union derived-tuples derived-tuples-for-rule))))))) ; Accumulate all derived tuples.
 
-  (define S* (stratify P))
-  ;(printf "stratify ~v\n\n" S*)
+  (define strata (stratify P))
+  ;(printf "stratify ~v\n\n" strata)
 
-  (let inter-loop ((E E) (S S*))
-    (printf "\ninter ~a/~a with ~a facts\n" (- (set-count S*) (set-count S)) (set-count S*) (set-count E))
-    (if (null? S)
-        E
-        (let ((Pi (car S)))
+  ; * E      set of facts (initially only the ones in the database)
+  ; * strata list of strata
+  (let inter-loop ((E-inter E) (S strata))
+    (printf "\ninter ~a/~a with ~a facts\n" (- (set-count strata) (set-count S)) (set-count strata) (set-count E-inter))
+    (if (null? S) ; Check whether there are more strata to traverse.
+        E-inter ; All facts (initial and derived).
+        (let ((Pi (car S))) ; Rules in the first stratum.
           (printf "Pi: ~v\n" (list->set (set-map Pi (lambda (r) (atom-name (rule-head r))))))
-          (let intra-loop ((E-intra E))
+          (let intra-loop ((E-intra E-inter))
             (let ((tuples (solve-naive-helper Pi E-intra)))
               (let ((E-intra* (set-union E-intra tuples)))
                   (if (equal? E-intra E-intra*) ; monotonicity: size check quicker? (TODO)
@@ -366,10 +369,10 @@
 
 (define (solve-semi-naive P E)
 
-  (define S* (stratify P))
+  (define strata (stratify P))
 
-  (let stratum-loop ((S S*) (E E))
-    (printf "\ninter ~a/~a with ~a facts\n" (- (set-count S*) (set-count S)) (set-count S*) (set-count E))
+  (let stratum-loop ((S strata) (E E))
+    (printf "\ninter ~a/~a with ~a facts\n" (- (set-count strata) (set-count S)) (set-count strata) (set-count E))
 
     (if (null? S)
         E
