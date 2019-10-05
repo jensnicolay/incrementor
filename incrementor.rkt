1fb5eb20b1d6c0d8b3ab2488f76258f17d9fa2e5
jnicolay <jens.nicolay@gmail.com>
2019-10-05 11:11:31 +0200

    factored out p->r and rule selection for tuples

--- incrementor.rkt
+++ incrementor.rkt
@@ -410,32 +410,40 @@
       (let ((rewrites (rewrite-rule r)))
         (set-union rewritten-rules rewrites)))))
 
+; in: rules
+; out: mapping pred -> rules with that pred in body
+(define (pred-to-rules rules)
+  (for/fold ((R (hash))) ((r (in-set rules)))
+    (for/fold ((R R)) ((term (in-list (rule-body r))))
+      (match term
+        ((¬ p)
+          (let ((term-name (atom-name p)))
+          (hash-set R term-name (set-add (hash-ref R term-name (set)) r)))) ; remember: no *Recent* possible!
+        (_ 
+          (let ((term-name (atom-name term)))
+            (if (equal? term-name '*Recent*)
+                (let ((recent-term (vector-ref term 1)))
+                  (let ((recent-name (atom-name recent-term)))
+                    (hash-set R recent-name (set-add (hash-ref R recent-name (set)) r))))
+                (hash-set R term-name (set-add (hash-ref R term-name (set)) r)))))
+        )))) ; always an EDB?
+
+; in: set of tuples and map of pred->rules
+; out: all rules that need to be fired (based on the preds in rules' body)
+(define (select-rules-for-tuples tuples p->r)
+  (for/fold ((R (set))) ((tuple (in-set tuples)))
+    (set-union R (hash-ref p->r (atom-name tuple) (set)))))
+
 
 (define (perform-iter rules tuples)
 
   (define semi-naive-rules (rewrite-semi-naive rules))
   (printf "semi-naive rules: ~a\ntuples: ~a\n" semi-naive-rules tuples)
-  (define pred-name-to-rules
-    (for/fold ((R (hash))) ((snr (in-set semi-naive-rules)))
-      (for/fold ((R R)) ((term (in-list (rule-body snr))))
-        (match term
-          ((¬ p)
-           (let ((term-name (atom-name p)))
-            (hash-set R term-name (set-add (hash-ref R term-name (set)) snr)))) ; remember: no *Recent* possible!
-          (_ 
-            (let ((term-name (atom-name term)))
-              (if (equal? term-name '*Recent*)
-                  (let ((recent-term (vector-ref term 1)))
-                    (let ((recent-name (atom-name recent-term)))
-                      (hash-set R recent-name (set-add (hash-ref R recent-name (set)) snr))))
-                  (hash-set R term-name (set-add (hash-ref R term-name (set)) snr)))))
-          )))) ; always an EDB?
-  ;(printf "pred-name-to-rules ~a\n" pred-name-to-rules)
+  (define p->r (pred-to-rules semi-naive-rules))
+  ;(printf "pred-to-rules ~a\n" p->r)
   
   (let rule-loop ((rules* semi-naive-rules) (tuples tuples) (previous-delta-tuples tuples) (delta-tuples (set)))
-    (let ((delta-rules
-            (for/fold ((R (set))) ((previous-delta-tuple (in-set previous-delta-tuples)))
-              (set-union R (hash-ref pred-name-to-rules (atom-name previous-delta-tuple) (set))))))
+    (let ((delta-rules (select-rules-for-tuples previous-delta-tuples p->r)))
       ;(printf "delta rules :~a\n" delta-rules)
       (let delta-rule-loop ((rules* delta-rules) (delta-tuples delta-tuples))
         (if (set-empty? rules*)
