b0a75c23a956ec2b05bbc3cb00c79a8797c9ef9e
jnicolay <jens.nicolay@gmail.com>
2020-07-06 16:48:33 +0200

    initial tests ragg

--- test-ragg.rkt
+++ test-ragg.rkt
@@ -3,49 +3,39 @@
 
 (require "datalog.rkt")
 (require "naive.rkt")
-;(require "semi-naive.rkt")
-;(require "incremental.rkt")
+(require "semi-naive.rkt")
+; (require "incremental.rkt")
 
 (define tests 0)
 (define errors 0)
 
 (define (report-error solver P E I S)
   (set! errors (add1 errors))
-  (printf "error ~a ~a\nP ~a\nI ~a\nS ~a\n\n" solver tests P (sort-tuples (set-union E I)) (sort-tuples S)))
+  (printf "error ~a ~a\nP ~a\nE ~a\nI ~a\nS ~a\n\n" solver tests P E (sort-tuples (set-union E I)) (sort-tuples S)))
 
-(define (test P I)
+(define (test P E I)
   (set! tests (add1 tests))
-  (define E (set #(I 123)))
   (define expected (set-union E I))
   (define Sn (solver-result-tuples (solve-naive P E)))
-  ;(define Ssn (solver-result-tuples (solve-semi-naive P E)))
-  ;(define Si (solver-result-tuples (solve-incremental P E)))
+  (define Ssn (solver-result-tuples (solve-semi-naive P E)))
+;   (define Si (solver-result-tuples (solve-incremental P E)))
 
   (unless (equal? expected Sn)
     (report-error 'naive P E I Sn))
 
-  ; (unless (equal? expected Ssn)
-  ;   (report-error 'semi-naive P E I Ssn))
+  (unless (equal? expected Ssn)
+    (report-error 'semi-naive P E I Ssn))
 
-  ; (unless (equal? expected Si)
-  ;   (report-error 'incremental P E I Si))
+;   (unless (equal? expected Si)
+;     (report-error 'incremental P E I Si))
 )
 
+(test (set (:- #(R g #:sum x) #(I g x)))
+      (set #(I a 1) #(I a 2) #(I b 33))
+      (set #(R a 3) #(R b 33)))
 
-(test (set (:- #(R y)   #(I y) #(= y 123)))
-      (set #(R 123)))
-
-(test (set (:- #(R y)   #(I y) #(= 123 y)))
-      (set #(R 123)))
-
-(test (set (:- #(R x)   #(I y) #(= x (+ y 2))))
-      (set #(R 125)))
-
-(test (set (:- #(R x)   #(I y) #(= x `#(T ,(+ y 2)))))
-      (set #(R #(T 125))))
-
-(test (set (:- #(R x)   #(I y) #(= #(T x) `#(T ,(+ y 2)))))
-      (set #(R 125)))
-
+(test (set (:- #(R g #:count x) #(I g x)))
+      (set #(I a 1) #(I a 2) #(I b 33))
+      (set #(R a 2) #(R b 1)))
 
 (printf "\n\nERRORS: ~a\n\n" errors)  
